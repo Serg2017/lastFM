ba8da924db0d52d5e5f321bb0a1804586fa5ab09
Medved <serg.medved22@gmail.com>
2017-09-20 17:51:55 +0300

    fixed show artists

--- src/app/artists/artist.component.ts
+++ src/app/artists/artist.component.ts
@@ -11,14 +11,24 @@ export class ArtistComponent implements OnInit {
 
     artistInfo: any = {};
     artistName: string;
+    name: string;
+    image: string;
+    form: any;
 
     constructor(private artistService: ArtistService) {}
+
     ngOnInit(): void {}
+
+    setProperty() {
+        this.name = this.artistInfo.name;
+        this.image = this.artistInfo.image[2]['#text'];
+    }
 
     getInfo() {
         this.artistInfo = this.artistService.getInfoArtist(this.artistName).subscribe(
             (data) => this.artistInfo = data.artist,
+            err => console.log('error'),
+            () => this.setProperty()
         );
     }
-
 }
